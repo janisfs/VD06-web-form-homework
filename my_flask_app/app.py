7e4fc94ba554cdb8a7bb435c7436a2d94dddc1e1
janisfs <janisfs@mail.ru>
2024-09-25 22:35:05 +0300

    add new file

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,6 +1,22 @@
-def main90:
-    pass
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
-if __main__ ='__main__':
-    main()
+@app.route("/", methods=["GET", "POST"])
+def index():
+    user_data = None
+    if request.method == "POST":
+        name = request.form.get("name")
+        city = request.form.get("city")
+        hobby = request.form.get("hobby")
+        age = request.form.get("age")
+        user_data = {
+            "name": name,
+            "city": city,
+            "hobby": hobby,
+            "age": age
+        }
+    return render_template("index-simple.html", user_data=user_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
